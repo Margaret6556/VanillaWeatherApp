e1d75d1cb4c600f1844c10e93c6b9c73ad48f724
cezarlofellem@gmail.com <99307243+Margaret6556@users.noreply.github.com>
2022-07-19 00:04:40 -0400

    Updated some JS codes and formatting from HTML
    Updated some JS codes and formatting from HTML

--- src/app.js
+++ src/app.js
@@ -1,24 +1,40 @@
-function showCity(response) {
-  let cityInputElement = document.querySelector("#city-input");
-}
-
 function showTemperature(response) {
   console.log(response.data.main.temp);
-  let temperatureElement.innerHTML = Math.round("#temperature");
 }
 
-let apiKey = "cd32822f9e64e59f7e2528e37e7549fa";
-let apiUrl =
-  `https://api.openweathermap.org/data/2.5/weather?q=toronto&appid=cd32822f9e64e59f7e2528e37e7549fa`;
-let units = "metrics";
-let temperature = "#temperature";
-let lat = position.coords.latitude;
-let lon = position.coords.longitude;
+function getPosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  console.log(lat, lon);
+}
 
-axios.get(apiUrl).then(ShowTemperature);
-navigator.geolocation.getCurrentPosition(retrievePosition);
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(retrievePosition);
+}
 
-let city = document.querySelector("form-control");
+function search(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  searchCity(cityInputElement.value);
+}
+
+function searchCity(city) {
+  let apiKey = "cd32822f9e64e59f7e2528e37e7549fa";
+  let units = "metrics";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+  console.log(cityInputElement.value);
+}
+
+let city = document.querySelector("#search-form");
 city.addEventListener("submit", handleSubmit);
 
-search("city");
+search("Toronto");
