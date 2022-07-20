d49bd8c97252be64db50c97fa7853492287c57b0
cezarlofellem@gmail.com <99307243+Margaret6556@users.noreply.github.com>
2022-07-20 00:19:47 -0400

    Updated JS codes

--- src/app.js
+++ src/app.js
@@ -1,16 +1,37 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes - `0${minutes}`;
+  }
+}
+
+let dayIndex = date.getDay();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[dayIndex];
+
+return newFunction();
+
+function newFunction() {
+  return `${day} ${hours}:${minutes}`;
+}
+
 function showTemperature(response) {
-  console.log(response.data.main.temp);
-}
-
-function getPosition(position) {
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  console.log(lat, lon);
-}
-
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(retrievePosition);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
 }
 
 function search(event) {
@@ -23,17 +44,30 @@ function searchCity(city) {
   let apiKey = "cd32822f9e64e59f7e2528e37e7549fa";
   let units = "metrics";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-
   axios.get(apiUrl).then(showTemperature);
 }
 
 function handleSubmit(event) {
   event.preventDefault();
-  let cityInputElement = document.querySelector("#city-input");
-  searchCity(cityInputElement.value);
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
 }
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(retrievePosition);
+}
+function getPosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  console.log(lat, lon);
+}
+
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
 
 let city = document.querySelector("#search-form");
 city.addEventListener("submit", handleSubmit);
 
-searchCity("San Francisco");
+searchCity("Paris");
